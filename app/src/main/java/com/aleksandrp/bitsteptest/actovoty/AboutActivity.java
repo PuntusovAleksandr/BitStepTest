bc0f16c4a145c9515e947ff73477e4e6d774912d
aleksandr_mdc@mail.ru <Mishe12345>
2017-09-26 13:13:17 +0300

    addded about programm

--- app/src/main/java/com/aleksandrp/bitsteptest/actovoty/AboutActivity.java
+++ app/src/main/java/com/aleksandrp/bitsteptest/actovoty/AboutActivity.java
@@ -2,17 +2,23 @@ package com.aleksandrp.bitsteptest.actovoty;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.aleksandrp.bitsteptest.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AboutActivity extends AppCompatActivity {
 
     @Bind(R.id.tv_title_toolbar)
     TextView tv_title_toolbar;
+
+    @Bind(R.id.iv_back)
+    ImageView iv_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,14 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
         ButterKnife.bind(this);
 
+        iv_back.setVisibility(View.VISIBLE);
         tv_title_toolbar.setText(getString(R.string.about));
+    }
+
+    //    ==================================================
+
+    @OnClick(R.id.iv_back)
+    public void iv_backClick() {
+        onBackPressed();
     }
 }
